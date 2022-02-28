6ccc15545f57acbcd3cffd707200d1b9685bb476
Agustin Carnerero Peña <agusticar@gmail.com>
2022-02-28 21:25:46 +0100

    TascaS4.01SpringInitialNivell2

---
+++ src/main/java/carnerero/agustin/S41/Nivell2/Application.java
@@ -0,0 +1,13 @@
+package carnerero.agustin.S41.Nivell2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
